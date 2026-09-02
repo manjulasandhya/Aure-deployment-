682b56835d1462d63f2d4dfa7558adde92f082f0
manjulasandhya <manjulamishra44@gmail.com>
2026-09-02 13:24:15 +0530

    sample deployment

---
+++ score.py
@@ -0,0 +1,20 @@
+import joblib
+import json
+import numpy as np
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json(force=True)
+        features = np.array(data['features']).reshape(1, -1)
+        prediction = model.predict(features)
+        return jsonify({'prediction': int(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
